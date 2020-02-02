98a1be17736a88a9fe71df01407c4384a9c39df1
thisisshub <thisisshub@gmail.com>
2020-02-02 18:27:50 +0530

    modified files and added templates

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from django.contrib.auth.admin import User
+from django.urls import reverse
 
+class Posts(models.Model):
+    uploader = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=2500)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts-detail", kwargs={"pk": self.pk})
